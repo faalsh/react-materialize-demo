ebb8fa876735087f9713aaea8b2f3fc790db0952
faalsh <fahad@Fahads-MacBook-Pro.local>
2016-09-15 22:31:36 +0200

    favicon

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var fallback = require('express-history-api-fallback');
+var favicon = require('serve-favicon');
 
 
 var app = express();
 
-app.set('port', (process.env.PORT || 80));
+app.set('port', (process.env.PORT || 5000));
 var root = `${__dirname}/src`;
 
 app.use(express.static(root));
-app.use(fallback('index.html',  {root} ))
-
+app.use(fallback('index.html',  {root} ));
+app.use(favicon(__dirname + '/src/favicon.ico'));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
