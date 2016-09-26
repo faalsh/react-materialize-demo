676a9d3ccbdd39fe4e328466f328d882b17ebf9f
faalsh <fhooodi@gmail.com>
2016-09-26 11:39:42 +0200

    leagues and seasons dropdown fixed

--- src/components/LeaguesDropDown/LeaguesDropDown.jsx
+++ src/components/LeaguesDropDown/LeaguesDropDown.jsx
@@ -4,53 +4,10 @@ import firebase from 'firebase'
 
 class LeaguesDropDown extends React.Component {
 
-	constructor(props) {
-		super(props)
-		this.state = {
-			leagues: []
-		}
-	}
-
-	componentDidMount() {
-	 	const config = {
-		    apiKey: "AIzaSyDka8fzhavzcKcu7NPBUo3m-Bn0RrlXk9Q",
-		    authDomain: "soccer-ca71d.firebaseapp.com",
-		    databaseURL: "https://soccer-ca71d.firebaseio.com",
-		    storageBucket: "soccer-ca71d.appspot.com",
-		    messagingSenderId: "263223690384"
-		};
-
-		firebase.initializeApp(config);
-
-		var leaguesRef = firebase.database().ref('/leagues/');
-		// console.log(leaguesRef)
-		// const database = firebase.database();
-		// const leaguesRef = database.ref('/leagues/');
-		leaguesRef.on('value', (snapshot) => {
-			// console.log(snapshot.hasChildren());
-			var leagues = [];
-			snapshot.forEach((league) => {
-				// console.log(league.key,league.val())
-				leagues.push({name: league.val().name, to:{pathname: '/matches', query: {league: league.val().identifier}}})
-			})
-			this.setState({
-				leagues: leagues
-			});
-		})
-     
-	}
 
     render() {
-        const {selectedName} = this.props
-
-
-        // const items = this.props.leagues.leagues.map((league) => {
-        //      return {name:league.name, to:{pathname: '/matches', query: {league: league.league_slug} }}
-        //  })
-
-        const items = this.state.leagues
-
-        return <DropDown id="leagues" name="Leagues" items={items} selectedName={selectedName}/>;
+        const {leagues, selectedName} = this.props
+        return <DropDown id="leagues" name="Leagues" items={leagues} selectedName={selectedName}/>;
     }
 }
 
--- src/containers/Matches/Matches.jsx
+++ src/containers/Matches/Matches.jsx
@@ -8,34 +8,92 @@ import LeaguesDropDown from '../../components/LeaguesDropDown/LeaguesDropDown.js
 import SeasonsDropDown from '../../components/SeasonsDropDown/SeasonsDropDown.jsx'
 import RoundsDropDown from '../../components/RoundsDropDown/RoundsDropDown.jsx'
 import MatchesTable from '../../components/MatchesTable/MatchesTable.jsx'
+import firebase from 'firebase'
 
 
 class Matches extends React.Component {
+    
+    constructor(props) {
+        super(props)
+        this.state = {
+            leagues: [],
+            seasons: [],
+            rounds: [],
+            matches: []
+        }
+    }
 
     componentDidMount() {
-      this.props.fetchLeagues() 
+      // this.props.fetchLeagues()
+
+      const config = {
+            apiKey: "AIzaSyDka8fzhavzcKcu7NPBUo3m-Bn0RrlXk9Q",
+            authDomain: "soccer-ca71d.firebaseapp.com",
+            databaseURL: "https://soccer-ca71d.firebaseio.com",
+            storageBucket: "soccer-ca71d.appspot.com",
+            messagingSenderId: "263223690384"
+        };
+
+        firebase.initializeApp(config);
+        this.getLeagues();
+        const {league} = this.props.location.query
+        if(league) {
+            this.getSeasons(league);
+        }
+
+        
+    }
+    componentWillReceiveProps(nextProps) {
+        const {league} = nextProps.location.query
+        if (league) {
+            this.getSeasons(league);
+        }     
+    }
+
+    getLeagues(){
+        var leaguesRef = firebase.database().ref('/leagues/');
+        leaguesRef.on('value', (snapshot) => {
+            var leagues = [];
+            snapshot.forEach((league) => {
+                leagues.push({name: league.val().name, to:{pathname: '/matches', query: {league: league.val().league_slug}}})
+            });
+            this.setState({
+                leagues: leagues
+            });
+        });
+    }
+
+    getSeasons(league) {
+        var seasonsRef = firebase.database().ref('/seasons/');
+        seasonsRef.orderByChild("league_slug").equalTo(league).on('value', (snapshot) => {
+            var seasons = [];
+            snapshot.forEach((season) => {
+                seasons.push({name: season.val().name, to:{pathname: '/matches', query: {league:league, season: season.val().season_slug}}})
+            });
+            this.setState({
+                seasons: seasons
+            });
+        });
     }
 
 
 	render() {
 
-        const {fetching, fetchSeasons, fetchRounds,fetchMatches, seasons, leagues, rounds, matches} = this.props
-        const {query} = this.props.location
+        const {leagues, seasons, rounds, matches} = this.state;
+        const {league} = this.props.location.query
 
     return (
 
             <div className="container">
             <div className="row valign-wrapper">
                 <div className="col s12 filters">
-                    <LeaguesDropDown leagues={leagues} selectedName={query.league} /> 
-                    <SeasonsDropDown fetchSeasons={fetchSeasons}  seasons={seasons} query={query} />
-                    <RoundsDropDown fetchRounds={fetchRounds} rounds={rounds} query={query} />
+                    <LeaguesDropDown leagues={leagues} /> 
+                    <SeasonsDropDown seasons={seasons} league={league}/>
                     
                 </div>
             </div>
 
             <div className="row">
-            <MatchesTable fetchMatches={fetchMatches} matches={matches} query={query}/>
             </div>
             </div>
 
@@ -44,25 +102,4 @@ class Matches extends React.Component {
 
 }
 
-
-function mapStateToProps(state){
-	return {
-        fetching: state.league.fetching || state.season.fetching || state.round.fetching,
-        leagues: state.league,
-        seasons: state.season,
-        rounds: state.round,
-        matches: state.match
-	}
-}
-
-function matchDispatchToProps(dispatch){
-	return bindActionCreators({
-            fetchLeagues: fetchLeagues,
-            fetchSeasons: fetchSeasons,
-            fetchRounds: fetchRounds,
-            fetchMatches: fetchMatches
-		}, dispatch);
-}
-
-
-export default connect(mapStateToProps, matchDispatchToProps)(Matches);
+export default Matches;
